54cc1b814c42f6e94d3c281a31a427bdeeb30cc2
Haruka Goto <harubj6999@gmail.com>
2019-06-22 18:23:54 +0900

    mいろんな機能

--- js/main.js
+++ js/main.js
@@ -1,9 +1,18 @@
 jQuery(function($){
   // リロード時にページtopへ
+  $('.contents').css({
+    'display':'none'
+  });
   $('html,body').animate({ scrollTop: 0 },  { duration: 1500});
+  setTimeout(function(){
+    $('.contents').css({
+      'display':'block'
+    });
+  },1000);
 
   // about,themeのfadeIn
-  jQuery(window).scroll(function (){
+  setTimeout(function(){
+    jQuery(window).scroll(function (){
       var elemPos1 = $('.about').offset().top;
       var elemPos2 = $('.theme').offset().top;
       var elemPos = new Array();
@@ -23,19 +32,20 @@ jQuery(function($){
           $('.line'+i).addClass('lineZoom');
         }
       }
-  });
+    });
+  },1000);
 
   // movieの文字スライド
   $("#movieText").ready(function(){  
     var i = 0;
-    var scroll = function(){
+    var slide = function(){
       $("#movieText").css({
         'transform':'matrix(1,0,0,1,'+ i +',0)'
       });
       i --;
     }
-    var id = setInterval(function(){
-      scroll();
+    setInterval(function(){
+      slide();
       if(i ==　-225){　
         // clearInterval(id);  
         i = 0;
@@ -45,15 +55,167 @@ jQuery(function($){
 
   // 部署長画像(丸)
   for(var i=3;i<=19;i++){
-    $('.memberImgBox').append('<div class="memberImgDiv"><img src="image/members/'+i+'.png" class="memberImg me'+i+'"></div>')
+    $('.memberImgBox').append('<div class="memberImgDiv"><img src="image/members/'+i+'.png" class="memberImg" id="me'+i+'"></div>')
     if(i%3==0 || i%3==2){ //左の列と右の列
 
     }else{ //真ん中の列
-      $('.me'+i).css({
+      $('#me'+i).css({
         'margin-top':'5vh'
       })
     }
   }
+
+  // 適部署バーガー
+  $("#tekibusho img").ready(function(){  
+    var i = 0;
+    var j = 0;
+    var x = 0;
+    var y = 0;
+    var moveX = function(){
+      i++;
+      x=i;
+      if(i>=85){
+        x = 85 - (i-85);
+      }
+      if(i==170){
+        i=0;
+        x=i;
+      }
+      $("#tekibusho img").css({
+        'left': x+'vw'
+      });
+    }
+    var moveY = function(){
+      j+=0.5;
+      y=j;
+      if(j>=95){
+        y = 95 - (j-95);
+      }
+      if(j==190){
+        j=0;
+        y=j;
+      }
+      $("#tekibusho img").css({
+        'top': y+'%'
+      });
+    }
+    setInterval(function(){
+      moveX();
+      moveY();
+    }, 80);
+  });
+
+  // popup
+  $('#close').click(function(){
+    $('.black').css({
+      'display':'none'
+    })
+    $('.popup').css({
+      'animation':'popsOut .5s'
+    })
+    setTimeout(function(){
+      $('.popup').css({
+        'display':'none',
+        'animation': 'none'
+      });
+    },500);
+  })
+  $('.memberImg').click(function(){
+    var meIdSp = $(this).attr('id') .split('me');
+    var meId = meIdSp[1];
+    $('.icon').attr("src","image/icon/"+meId+".png");
+    $('.bushocho').attr("src","image/bushocho/"+meId+".png");
+    switch(meId){
+      case "1":
+        $('.bushomei').text('芸工祭長');
+        $('.name').text('パッション');
+      break;
+      case "2":
+        $('.bushomei').text('副祭長');
+        $('.name').text('ジャーヴィス');
+      break;
+      case "3":
+        $('.bushomei').text('書記');
+        $('.name').text('ハヤミ');
+      break;
+      case "4":
+        $('.bushomei').text('総務');
+        $('.name').text('フラット');
+      break;
+      case "5":
+        $('.bushomei').text('会計');
+        $('.name').text('さくら');
+      break;
+      case "6":
+        $('.bushomei').text('web部署長');
+        $('.name').text('うーさー');
+      break;
+      case "7":
+        $('.bushomei').text('映像部署長');
+        $('.name').text('ざわくん');
+      break;
+      case "8":
+        $('.bushomei').text('グラフィック部署長');
+        $('.name').text('マルメ');
+      break;
+      case "9":
+        $('.bushomei').text('インテリア部署長');
+        $('.name').text('ひざ');
+      break;
+      case "10":
+        $('.bushomei').text('エクステリア部署長');
+        $('.name').text('てら');
+      break;
+      case "11":
+        $('.bushomei').text('ディレクション部署長');
+        $('.name').text('アルジャ');
+      break;
+      case "12":
+        $('.bushomei').text('ブース部署長');
+        $('.name').text('ミーシャ');
+      break;
+      case "13":
+        $('.bushomei').text('バー部署長');
+        $('.name').text('クロール');
+      break;
+      case "14":
+        $('.bushomei').text('ワークショップ部署長');
+        $('.name').text('枝豆納豆二人セゾン');
+      break;
+      case "15":
+        $('.bushomei').text('昼ステ長');
+        $('.name').text('鋤骨');
+      break;
+      case "16":
+        $('.bushomei').text('夜ステ長');
+        $('.name').text('ボマイェ');
+      break;
+      case "17":
+        $('.bushomei').text('カフェ部署長');
+        $('.name').text('ごま');
+      break;
+      case "18":
+        $('.bushomei').text('PA部署長');
+        $('.name').text('ドナさん');
+      break;
+      case "19":
+        $('.bushomei').text('運動会部署長');
+        $('.name').text('ジョニー');
+      break;
+    }
+    $('.black').css({
+      'display':'inline'
+    })
+    $('.popup').css({
+      'display':'inline',
+      'animation':'popsIn .5s'
+    })
+    setTimeout(function(){
+      $('.popup').css({
+        'animation': 'none'
+      });
+    },500);
+  })
   
 
 });
